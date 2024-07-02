5dae280a10888f69b513696247c511a57bee1990
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-02 13:44:10 -0400

    Beans are created.

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/company/streotype_annotation/monitorfactory/SonyMonitor.java
@@ -0,0 +1,16 @@
+package com.company.streotype_annotation.monitorfactory;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class SonyMonitor extends Monitor{
+
+    public SonyMonitor() {
+        super("25 inch beast","Sony",25);
+    }
+
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+    }
+}
