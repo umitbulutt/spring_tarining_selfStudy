3cb7a93b2c76d58256c867c01c54c04cb8e53ba6
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-03 09:50:10 -0400

    Dabase Layer is done. Abstract method has been implemented by EmailCommentNotificationProxy and PushCommentNotificationProxy classes.

---
+++ spring_05_Core_Abstraction/src/main/java/com/company/service/CommentService.java
@@ -0,0 +1,29 @@
+package com.company.service;
+
+import com.company.model.Comment;
+import com.company.proxy.CommentNotificationProxy;
+import com.company.repository.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        //save in the DB
+            commentRepository.storeComment(comment);
+
+        //send email
+
+        commentNotificationProxy.sendComment(comment);
+
+    }
+}
