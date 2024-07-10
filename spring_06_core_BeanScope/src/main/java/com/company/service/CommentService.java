ad5f1053851fbc0741e210994d6daf8e4e01a693
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-09 21:17:15 -0400

    BeanScope relation  has done.

---
+++ spring_06_core_BeanScope/src/main/java/com/company/service/CommentService.java
@@ -0,0 +1,31 @@
+package com.company.service;
+
+import com.company.model.Comment;
+import com.company.proxy.CommentNotificationProxy;
+import com.company.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        //save in the DB
+            commentRepository.storeComment(comment);
+
+        //send email
+
+        commentNotificationProxy.sendComment(comment);
+
+    }
+}
