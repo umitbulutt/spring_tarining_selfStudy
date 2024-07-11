743393a452de5c9fd271e70ec34fe7803a617c9b
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-11 18:26:14 -0400

    Mentor Controller and mapping is done by register and confirm method

---
+++ spring_10_Thymeleaf/src/main/java/com/company/controller/MentorController.java
@@ -0,0 +1,44 @@
+package com.company.controller;
+
+import com.company.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    //localhost:8080/mentor/register
+    @GetMapping("/register")
+    public String register(Model model){
+
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+
+        model.addAttribute("mentor",new Mentor());
+
+        return "mentor/mentor-register";
+    }
+
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+ //       return "mentor/mentor-confirmation";
+
+
+        return "redirect:/mentor/register";
+    }
+
+
+
+
+
+}
