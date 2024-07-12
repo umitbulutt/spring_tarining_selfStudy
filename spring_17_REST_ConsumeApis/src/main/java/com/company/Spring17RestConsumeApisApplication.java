5f3753074e638fbe0574b842fb1c1790e26e38d9
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-11 21:54:08 -0400

    Consuming Api`s module has created.

---
+++ spring_17_REST_ConsumeApis/src/main/java/com/company/Spring17RestConsumeApisApplication.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Spring17RestConsumeApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumeApisApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
