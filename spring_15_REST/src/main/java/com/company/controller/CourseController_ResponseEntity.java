a6a1a54e3aa15bfe3f83d19ec9f925389e4aafc6
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-11 21:41:34 -0400

    Delete method is created.

--- spring_15_REST/src/main/java/com/company/controller/CourseController_ResponseEntity.java
+++ spring_15_REST/src/main/java/com/company/controller/CourseController_ResponseEntity.java
@@ -53,6 +53,18 @@ public class CourseController_ResponseEntity {
 
     }
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCourse(@PathVariable("id") Long courseId,@RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
