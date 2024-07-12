563c119bd4a92c48a2253782e07bfaa037eeb5c3
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-11 20:22:43 -0400

    For customize Json result ResponseWrapper class has created.

---
+++ spring_15_REST/src/main/java/com/company/dto/ResponseWrapper.java
@@ -0,0 +1,23 @@
+package com.company.dto;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseWrapper {
+
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message,Object data){
+        this.message = message;
+        this.data = data;
+        this.code  = HttpStatus.OK.value();
+        this.success = true;
+    }
+    public ResponseWrapper(String message){
+        this.message =  message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+}
