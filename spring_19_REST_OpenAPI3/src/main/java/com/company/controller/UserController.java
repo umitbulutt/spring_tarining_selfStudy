bd8891a510b227a13ae5f22ea46f3d8dd17f3da1
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-14 23:26:04 -0400

    OpenAPI3 ; Basic UserController class has created.

---
+++ spring_19_REST_OpenAPI3/src/main/java/com/company/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.company.controller;
+
+import com.company.dto.UserDTO;
+import com.company.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+  @PostMapping
+      public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userDTO));
+    }
+
+}
