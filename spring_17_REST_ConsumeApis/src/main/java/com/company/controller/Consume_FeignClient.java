34fe0c82d57e8a2ca14b57f89fff3d7f4e8046bd
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-14 23:21:12 -0400

    OpenAPI3 ; Consuming the employee by endpoint with FeignClient is done.

--- spring_17_REST_ConsumeApis/src/main/java/com/company/controller/Consume_FeignClient.java
+++ spring_17_REST_ConsumeApis/src/main/java/com/company/controller/Consume_FeignClient.java
@@ -4,9 +4,12 @@ package com.company.controller;
 import com.company.client.EmployeeClient;
 import com.company.client.UserClient;
 import com.company.dto.ResponseWrapper;
+import com.company.dto.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Consume_FeignClient {
@@ -22,9 +25,9 @@ public class Consume_FeignClient {
     }
 
     @GetMapping("/api/v1/users")
-    public ResponseEntity<ResponseWrapper> getUsers(){
+    public ResponseEntity<List<User>> getUsers(){
 
-        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved",userClient.getUsers()));
+        return ResponseEntity.ok(userClient.getUsers());
     }
 
 
