13f3a101b2e4dbcd462621d836b0366d679920a7
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-17 19:00:54 -0400

    Logger object has created.

--- spring_21_AOP/src/main/java/com/company/aspect/LoggingAspect.java
+++ spring_21_AOP/src/main/java/com/company/aspect/LoggingAspect.java
@@ -4,6 +4,8 @@ package com.company.aspect;
 
 import org.aspectj.lang.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.List;
 @Aspect
 @Component
 public class LoggingAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
 
 
