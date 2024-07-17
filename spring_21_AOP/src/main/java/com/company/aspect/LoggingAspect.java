0afbec929c9de6e602866cb424d636f18ceadeab
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-17 19:27:09 -0400

    First joinpoint implementation is done.

--- spring_21_AOP/src/main/java/com/company/aspect/LoggingAspect.java
+++ spring_21_AOP/src/main/java/com/company/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.company.aspect;
 
 
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 
 import org.slf4j.Logger;
@@ -17,12 +18,28 @@ public class LoggingAspect {
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
 
-    @Pointcut("execution(* com.company.controller.CourseController.*(..))")   //Defining my joinPoint
-    public void myPointcut(){}
+ //   @Pointcut("execution(* com.company.controller.CourseController.*(..))")   //Defining my joinPoint
+ //   public void myPointcut(){}
+//
+ //   @Before("myPointcut()")
+ //   public void log(){
+ //       logger.info("Info log........");
+ //   }
+//
 
-    @Before("myPointcut()")
-    public void log(){
-        logger.info("Info log........");
+//    @Before("execution(* com.company.controller.CourseController.*(..))")   //Defining my joinPoint
+//    public void log(){
+//        logger.info("Info log.................");
+//    }
+
+    @Pointcut("execution(* com.company.repository.CourseRepository.findById(*))")
+    public void courseRepositoryFindByIdPC(){}
+
+
+    @Before("courseRepositoryFindByIdPC()")
+    public void beforeFindCourseRepositoryById(JoinPoint joinPoint){  //JoinPoint are the method we wanted to implement before or after
+        logger.info("Before : Method: {},Arguments: {}, Target: {}"
+                , joinPoint.getSignature(), joinPoint.getArgs(),joinPoint.getTarget());
     }
 
 }
