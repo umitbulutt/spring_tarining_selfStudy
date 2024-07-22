ca9ad378dfd0151d0e5b76c8943a74581fa78cd3
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-21 21:12:37 -0400

    Mockito Implementation - ProjectServiceImpl class created. ProjectRepository, ProjectMapper are mocked to ProjectServiceImpl.

---
+++ spring_23_UnitTesting_Mockito/src/test/java/com/company/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,34 @@
+package com.company.service.impl;
+
+import com.company.dto.ProjectDTO;
+import com.company.entity.Project;
+import com.company.mapper.ProjectMapper;
+import com.company.repository.ProjectRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceImplTest {
+
+    @Mock
+    ProjectRepository projectRepository;
+
+    @Mock
+    ProjectMapper projectMapper;
+
+    @InjectMocks
+    ProjectServiceImpl projectService;
+
+
+
+
+}
