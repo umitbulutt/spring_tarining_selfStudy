1a976a2fd19c2a155684fd2a7fdbf98c09ac6960
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-21 21:01:11 -0400

    Mockito Implementation - astLeast() and atLeastOnce() method has implemented.

--- spring_23_UnitTesting_Mockito/src/test/java/com/company/service/impl/UserServiceImplTest.java
+++ spring_23_UnitTesting_Mockito/src/test/java/com/company/service/impl/UserServiceImplTest.java
@@ -4,13 +4,13 @@ import com.company.mapper.UserMapper;
 import com.company.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)  //this is how you implement mockito class
@@ -35,6 +35,9 @@ class UserServiceImplTest {
         verify(userRepository).findByUserNameAndIsDeleted("harold@manager.com",false);
 
         verify(userRepository, times(1)).findByUserNameAndIsDeleted("harold@manager.com", false);
+
+        verify(userRepository, atLeast(1)).findByUserNameAndIsDeleted("harold@manager.com", false);
+        verify(userRepository, atLeastOnce()).findByUserNameAndIsDeleted("harold@manager.com", false);
 
 
 
